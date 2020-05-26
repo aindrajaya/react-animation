eb1d9349af6ba18772e3a01f9910da77886f39b8
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-05-26 15:07:29 +0700

    add some svg animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import logo1 from "../images/logo-sketch.png"
+import logo2 from "../images/logo-figma.png"
+import logo3 from "../images/logo-studio.png"
+import logo4 from "../images/logo-framer.png"
+import logo5 from "../images/logo-react.png"
+import logo6 from "../images/logo-swift.png"
+import wall1 from "../images/wallpaper2.jpg"
+import wall2 from "../images/wallpaper4.jpg"
+import wall3 from "../images/wallpaper.jpg"
 import SEO from "../components/seo"
+import Card from "../components/Card"
 
 const IndexPage = () => (
   <Layout>
@@ -12,8 +21,40 @@ const IndexPage = () => (
         <h1>Learn to Design and Code React Apps</h1>
         <p>Welcome to your new Gatsby site annaasas.</p>
         <Link to="/page-2/">Go to page 2</Link>
+        <div className="Logos">
+          <img src={logo1} width="50"/>
+          <img src={logo2} width="50"/>
+          <img src={logo3} width="50"/>
+          <img src={logo4} width="50"/>
+          <img src={logo5} width="50"/>
+          <img src={logo6} width="50"/>
+        </div>
+        <svg width="100%" 
+          height="172"  
+          fill="none" 
+          xmlns="http://www.w3.rg/2000/svg">
+          <path fill="white">
+            <animate repeatCount="indefinite" 
+              fill="freeze" 
+              attributeName="d" 
+              dur="10s" 
+              values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+                M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
+                M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
+                M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z"
+              />
+          </path>
+        </svg>
       </div>
     </div>
+    <div className="Cards">
+        <h2>11 Courses, more coming</h2>
+        <Card 
+          title="React for Designers"
+          text="12 sections"
+          image={wall1}
+        />
+      </div>
   </Layout>
 )
 
